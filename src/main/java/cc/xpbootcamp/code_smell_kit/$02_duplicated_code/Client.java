7c8fdf80426d010c8d79b2dc483ab07716e289cd
Qing Chen <qing.chen@thoughtworks.com>
2020-02-25 20:30:23 +0800

    format code

--- src/main/java/cc/xpbootcamp/code_smell_kit/$02_duplicated_code/Client.java
+++ src/main/java/cc/xpbootcamp/code_smell_kit/$02_duplicated_code/Client.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 
 public class Client {
 
-    ArrayList<ProductItem> products=new ArrayList();
+    ArrayList<ProductItem> products = new ArrayList();
 
-    public void addProductItem(ProductItem productItem){
+    public void addProductItem(ProductItem productItem) {
         products.add(productItem);
     }
 
-    public ArrayList chooseProductAboveSpecifiedPrice(double price){
-        ArrayList tempList=new ArrayList();
+    public ArrayList chooseProductAboveSpecifiedPrice(double price) {
+        ArrayList tempList = new ArrayList();
         for (int i = 0; i < products.size(); i++) {
             ProductItem productItem = products.get(i);
-            if(productItem.getPrice()>price){
+            if (productItem.getPrice() > price) {
                 tempList.add(productItem);
             }
         }
         return tempList;
     }
 
-    public ArrayList chooseProductBelowSpecifiedPrice(double price){
-        ArrayList tempList=new ArrayList();
+    public ArrayList chooseProductBelowSpecifiedPrice(double price) {
+        ArrayList tempList = new ArrayList();
         for (int i = 0; i < products.size(); i++) {
             ProductItem productItem = products.get(i);
-            if(productItem.getPrice()<price){
+            if (productItem.getPrice() < price) {
                 tempList.add(productItem);
             }
         }
