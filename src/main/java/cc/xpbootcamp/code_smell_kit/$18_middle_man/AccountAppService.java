98a0cc9dadef482612895c978c9ed976cd0b9129
superPershing <727342027@qq.com>
2020-02-26 20:09:13 +0800

    8 9 add bad smell comment

--- src/main/java/cc/xpbootcamp/code_smell_kit/$18_middle_man/AccountAppService.java
+++ src/main/java/cc/xpbootcamp/code_smell_kit/$18_middle_man/AccountAppService.java
@@ -1,5 +1,9 @@
 package cc.xpbootcamp.code_smell_kit.$18_middle_man;
 
+/**
+ * 坏味道: 中间人
+ * AccountAppService只是在调用AccountService所有方法，其表现的像一个中间人。
+ */
 public class AccountAppService {
     private AccountService accountService;
 
--- src/main/java/cc/xpbootcamp/code_smell_kit/$19_insider_trading/AccountRegister.java
+++ src/main/java/cc/xpbootcamp/code_smell_kit/$19_insider_trading/AccountRegister.java
@@ -3,6 +3,11 @@ package cc.xpbootcamp.code_smell_kit.$19_insider_trading;
 
 import java.util.List;
 
+/**
+ * 坏味道：内幕交易
+ * AccountRegister发送短信需要调用RegisterReport的generateReport方法，而RegisterReport的generateReport有需要使用
+ * AccountRegister的getAccounts方法，双方的方法互相调用耦合在一起。
+ */
 public class AccountRegister {
     private RegisterReport registerReport;
     private SmsClient smsClient;
